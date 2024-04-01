cdff8234d71392c68cb3c665704f663667efe297
earnstein <bdamilola00@gmail.com>
2024-04-01 22:31:04 +0100

    chore: move all posts to home view

--- blog/views.py
+++ blog/views.py
@@ -4,22 +4,18 @@ from django.shortcuts import render, get_object_or_404
 
 
 def home(request):
-    return render(request, 'home.html')
-
-def post_list_view(request):
     posts = Post.objects.all()
     published_posts = Post.published.all()
     draft_posts = Post.drafted.all()
     context = {
         'posts': posts,
         'published_posts': published_posts,
-        'draft_posts': draft_posts
+        'drafted_posts': draft_posts
     }
-    return render(request, 'blog/posts.html', context)
+    return render(request, 'home.html', context)
 
-
-def post_detail_view(request, post_id):
-    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+def post_detail_view(request, id):
+    post = get_object_or_404(Post, id=id)
     context = {'post': post}
     return render(request, 'blog/details.html', context)
 
