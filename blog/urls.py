cdb370f5e13dc025e68223f70da54e68fa078b7c
earnstein <bdamilola00@gmail.com>
2024-06-15 12:47:38 +0100

    chore: refactor url for FBV

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path("", views.HomeView.as_view(), name="home"),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('<int:post_id>/share/', views.PostShareView.as_view(), name='post_share'),
+    path("", views.home, name="home"),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail_view, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
