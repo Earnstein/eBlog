6bda29ad1e047b0613a235292b177c1003f02e0f
earnstein <bdamilola00@gmail.com>
2024-06-21 00:13:04 +0100

    chore: add taggit to installed apps

--- eBlog/settings.py
+++ eBlog/settings.py
@@ -27,8 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
     'compressor',
-    'blog'
+    'taggit',
 ]
 
 MIDDLEWARE = [
