988f96652112a10edb99267c211ebd77610b0abe
earnstein <bdamilola00@gmail.com>
2024-06-21 00:11:53 +0100

    chore: change home view to chore tag posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path('tag/<slug:tag_slug>/', views.home, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail_view, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
