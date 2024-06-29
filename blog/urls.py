3ef63025aec9b1e9da156af13aeb8f9f99c19fa4
earnstein <bdamilola00@gmail.com>
2024-06-29 15:04:59 +0100

    add: feed endpoint

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .feeds import LatestPostFeeds
 from . import views
 
 app_name = "blog"
@@ -9,4 +10,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail_view, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('feed/', LatestPostFeeds(), name="post_feed")
 ]
